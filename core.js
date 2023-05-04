49414f3e2434bb87916a0512f584c08ebab5429b
KappaNone <121200875+KappaNone@users.noreply.github.com>
2023-05-04 18:26:42 +0300

    Final things

--- core.js
+++ core.js
@@ -1,10 +1,10 @@
 function changeTheme() {
-    const image = document.querySelector('.container');
-    if (image.style.backgroundImage == 'url("Images/BackGround.jpg")') {
-        image.style.backgroundImage = 'url("Images/Space.jpg")'
-    } else {
-        image.style.backgroundImage = 'url("Images/BackGround.jpg")'
-    }
+	const image = document.querySelector('.container');
+	if (image.style.backgroundImage == 'url("Images/BackGround.jpg")') {
+		image.style.backgroundImage = 'url("Images/Space.jpg")'
+	} else {
+		image.style.backgroundImage = 'url("Images/BackGround.jpg")'
+	}
 }
 
 const TOKEN = "6167387338:AAERGfrnEzABZ40ui0ir7uzMkJDLi5tN05o";
@@ -16,27 +16,27 @@ const message = document.getElementById('message')
 
 
 document.getElementById('mail-form').addEventListener('submit', function(e) {
-    e.preventDefault();
+	e.preventDefault();
 
-    let tg_message = `<b>Сообщение с сата!</b>\n`;
-    tg_message += `<b>Отправитель: </b> ${ name.value } \n`;
-    tg_message += `<b>Почта отправителя: </b> ${ email.value } \n`;
-    tg_message += `<b>Сообщение: </b> ${ message.value } \n`;
+	let tg_message = `<b>Сообщение с сата!</b>\n`;
+	tg_message += `<b>Отправитель: </b> ${ name.value } \n`;
+	tg_message += `<b>Почта отправителя: </b> ${ email.value } \n`;
+	tg_message += `<b>Сообщение: </b> ${ message.value } \n`;
 
-    axios.post(URL_API, {
-        chat_id: CHAT_ID,
-        parse_mode: 'html',
-        text: tg_message
-    })
-    .then((res) => {
-        name.value = "";
-        email.value = "";
-        message.value = "";
-    })
-    .catch((err) => {
-        console.warn(err)
-    })
-    .finally(() => {
-        console.log('End')
-    })
+	axios.post(URL_API, {
+		chat_id: CHAT_ID,
+		parse_mode: 'html',
+		text: tg_message
+	})
+	.then((res) => {
+		name.value = "";
+		email.value = "";
+		message.value = "";
+	})
+	.catch((err) => {
+		console.warn(err)
+	})
+	.finally(() => {
+		console.log('End')
+	})
 })
