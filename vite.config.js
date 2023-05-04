0769bae51b5c591272239841e6e8fb4341cdf2cd
KappaNone <121200875+KappaNone@users.noreply.github.com>
2023-05-05 01:38:47 +0300

    Fix

---
+++ vite.config.js
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'About.html'),
+        contact: resolve(__dirname, 'Contact.html')
+      },
+    },
+  }
+})
